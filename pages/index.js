7817325909ab78c23becc5e4db5f145551fcd8d9
Victor <victor@aluno.ufrb.edu.br>
2022-01-25 21:14:59 -0300

    Página inicial dinâmica

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,7 @@
+import React from 'react';
 import appConfig from "../config.json";
 import { Box, Button, Text, TextField, Image } from "@skynexui/components";
-function GlobalStyle() {
-  return (
-    <style global jsx>{`
-      * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
-        list-style: none;
-      }
-      body {
-        font-family: "Open Sans", sans-serif;
-      }
-      /* App fit Height */
-      html,
-      body,
-      #__next {
-        min-height: 100vh;
-        display: flex;
-        flex: 1;
-      }
-      #__next {
-        flex: 1;
-      }
-      #__next > * {
-        flex: 1;
-      }
-      /* ./App fit Height */
-    `}</style>
-  );
-}
+import {useRouter} from 'next/router';
 
 function Titulo(props) {
   const Tag = props.tag || 'h1';
@@ -60,11 +32,10 @@ function Titulo(props) {
 //export default HomePage
 
 export default function PaginaInicial() {
-  const username = "vitucs";
-
+  const [username, setUsername] = React.useState('vitucs');
+  const roteamento = useRouter();
   return (
     <>
-      <GlobalStyle />
       <Box
         styleSheet={{
           display: "flex",
@@ -99,6 +70,10 @@ export default function PaginaInicial() {
           {/* Formulário */}
           <Box
             as="form"
+            onSubmit={(e)=>{
+                e.preventDefault();
+                roteamento.push('/chat');
+            }}
             styleSheet={{
               display: "flex",
               flexDirection: "column",
@@ -121,6 +96,7 @@ export default function PaginaInicial() {
             </Text>
 
             <TextField
+              value={username}
               fullWidth
               textFieldColors={{
                 neutral: {
@@ -129,6 +105,9 @@ export default function PaginaInicial() {
                   mainColorHighlight: appConfig.theme.colors.primary[500],
                   backgroundColor: appConfig.theme.colors.neutrals[800],
                 },
+              }}
+              onChange={ (event)=>{
+                  setUsername(event.target.value);
               }}
             />
             <Button
